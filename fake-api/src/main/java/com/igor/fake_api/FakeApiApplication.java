d6b63bf1dad4944ab5dae39d739c2c1ba1afc5b0
Pichau <igorof06@outlook.com>
2024-08-30 11:34:38 -0300

    Enale feign client  in spring boot application

--- fake-api/src/main/java/com/igor/fake_api/FakeApiApplication.java
+++ fake-api/src/main/java/com/igor/fake_api/FakeApiApplication.java
@@ -2,8 +2,10 @@ package com.igor.fake_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class FakeApiApplication {
 
 	public static void main(String[] args) {
